bbcda96e8ecea4184261234dc6949c2402886be9
Arthur Renié <renie.arthur@gmail.com>
2025-12-10 11:48:36 +0100

    Update script.js

--- workspace/script.js
+++ workspace/script.js
@@ -1,1 +1,8 @@
-console.log("test")
+let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
+db=db.getSiblingDB("formateur")
+
+const inserted = db.students.insertOne({
+    name:"Amaury"
+})
+
+console.log(inserted)
