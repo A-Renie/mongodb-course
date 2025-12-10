b58a42e8d07da8e98c0f18c050727f126cceeed8
Arthur Renié <renie.arthur@gmail.com>
2025-12-10 15:07:30 +0100

    Update script.js

--- workspace/script.js
+++ workspace/script.js
@@ -4,11 +4,9 @@ db = db.getSiblingDB('sample_mflix');
 
 // ==================FILMS AVEC KEANU
 // const movies = db.movies
-// .find({ cast:"Keanu Reeves"
-
-// },
-
-// )
+// .find({ 
+// cast:"Keanu Reeves"
+// })
 // .projection({
 //     title: true,
 //     year: true,
@@ -140,5 +138,35 @@ const movies = db.movies
 .limit(100);
 
 
-console.log(movies);
+// console.log(movies);
+
+// let db = connect("mongodb://root:test123@localhost:27017?authSource=admin");
+
+// db = db.getSiblingDB('technocite');
+
+
+// db.students.updateOne({
+//     _id: Object("....")
+// }, {
+//     $set: {
+//         courses: ["Java", "C#", "Mysql"]
+//     }
+// });
+
+// db = db.getSiblingDB('technocite');
+
+// const students = db.students.find();
+
+// const premierEtudiantId = students.toArray()[0]._id;
+
+// // UPDATE students SET courses = ["Java", "C#", "Mysql"]
+// const updateResult = db.students.updateOne({
+//     _id: premierEtudiantId
+// }, {
+//     $set: {
+//         courses: ["Java", "C#", "Mysql"]
+//     }
+// });
+
+// console.log(updateResult);
 
